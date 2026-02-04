934f5f73ec84e8580dbac862c82846fa3cd54a3d
jiwei <jiwei@roborock.com>
2026-02-04 17:30:04 +0800

    feat: spring boot admin server

---
+++ src/main/java/com/roborock/sbadminserver/SbAdminServerApplication.java
@@ -0,0 +1,15 @@
+package com.roborock.sbadminserver;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class SbAdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbAdminServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/roborock/sbadminserver/SbAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roborock.sbadminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbAdminServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
